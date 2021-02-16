16c35c1d7ec054c4e0776de806aedb4606224e6f
sophia-jihye <jihyeparkk@naver.com>
2021-02-16 13:57:06 +0900

    GSAD

--- datasets/GasSensorArrayDriftDataset.py
+++ datasets/GasSensorArrayDriftDataset.py
@@ -13,17 +13,28 @@ class GasSensorArrayDriftDataset():
         X, y = self.get_X_y(df)
         self.train_data, self.test_data, self.train_labels, self.test_labels = train_test_split(X, y, test_size=0.2, stratify=y, random_state=RANDOM_STATE)
         
-    def load_data(self, data_dir, dataset_id):
+    def load_data(self, data_dir, dataset_id):        
         filepath = os.path.join(data_dir, 'batch{}.dat'.format(dataset_id))
         print('Loading {}..'.format(filepath))
         df = pd.read_table(filepath, sep="\s+", header=None)
         for col in df.columns[1:]:
             df[col] = df[col].apply(lambda x: x.split(':')[-1])
-        return df
+            
+        def list_to_2d_array(list_x, sensor_num):
+            x = np.array(list_x)
+            x_new = x.reshape((sensor_num, -1))
+            return x_new.transpose()
+        
+        faults = df[df.columns[0]]
+        df = df[df.columns[1:]]
+        results = df.apply(lambda x: x.dropna().tolist(), axis=1).tolist()
+        matrix_df = pd.DataFrame({'sensor*time': results, 'fault': faults})
+        matrix_df['sensors'] = matrix_df['sensor*time'].apply(lambda x: list_to_2d_array(x, 16))
+        return matrix_df
     
     def get_X_y(self, df):
-        X = df[df.columns[1:]].values.astype(np.float32)
-        X = np.expand_dims(X, axis=0)
-        X = np.swapaxes(X,0,1)
-        y = df[0].values.astype(np.float32)
+        X = np.expand_dims(df['sensors'].values[0], axis=0)
+        for arr in tqdm(df['sensors'].values[1:]):
+            X = np.vstack([X, np.expand_dims(arr, axis=0)]).astype(np.float32)   # (161, 8, 16)
+        y = df['fault'].values.astype(np.float32)
         return X, y
